2018cfd70647673c26ec10ecd66163e10db443c5
xuejuncheng0222 <3028138995@qq.com>
2023-04-28 11:31:27 +0800

    使用组件库

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 //注册路由
 import router from './router'
+//引入组件库
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+
+Vue.use(ElementUI);//全局引入组件库
 
 Vue.config.productionTip = false
 
